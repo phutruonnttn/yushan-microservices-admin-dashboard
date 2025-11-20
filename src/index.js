25f9ff0b6ac13d2410b7af15da86d752cceaa697
Nguyễn Trường <truongnguyen@TruongNPs-MacPro.local>
2025-11-20 13:52:05 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { ConfigProvider, App as AntApp } from 'antd';
 
+// Initialize app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
